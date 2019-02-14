5afdd02e0f16bd884fb8d08c97a3da8b9ba28dd9
samiksha <munakarmisamiksha@gmail.com>
2019-02-14 11:12:53 +0545

    added kathmanducontroller and view

---
+++ GitPractice/Controllers/CollageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitPractice.Controllers
+{
+    public class CollageController : Controller
+    {
+        // GET: Collage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
